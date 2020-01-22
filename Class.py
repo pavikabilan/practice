e8a45ba50f40a67945425a4fcbcba39eb9987c52
pavithra <47311025+pavikabilan@users.noreply.github.com>
2020-01-22 16:46:08 +0530

    Update Class.py

--- Class.py
+++ Class.py
@@ -9,3 +9,16 @@ class Reverse():
         print(self.rev)    
 obj=Reverse()
 obj.reverse()
+
+''' sum of the digit'''
+class Sum():
+    def sum(self):
+        self.sum=0
+        self.n=int(input("enter the value: "))
+        while(self.n>0):
+            self.d=self.n%10
+            self.sum=self.sum+self.d 
+            self.n=self.n//10
+        print(self.sum)    
+obj=Sum()
+obj.sum()
